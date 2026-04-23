0c8d4ea64b2a931abe6baae7c8d55fa92ece0877
Sarusman <sarusmandu92@gmail.com>
2026-04-23 21:18:28 +0200

    Typo test

--- MyService/src/test/java/com/example/myservice/controllers/RentServiceRestTest.java
+++ MyService/src/test/java/com/example/myservice/controllers/RentServiceRestTest.java
@@ -10,8 +10,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class RentServiceRestTest {
--- MyService/src/test/java/com/example/myservice/services/CarServiceTest.java
+++ MyService/src/test/java/com/example/myservice/services/CarServiceTest.java
@@ -3,7 +3,9 @@ package com.example.myservice.services;
 import com.example.myservice.entities.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CarServiceTest {
 
